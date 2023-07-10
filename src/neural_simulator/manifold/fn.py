3b8481fc8939b2f0edc769c89eae6024aea45737
Felix Pei <64850082+felixp8@users.noreply.github.com>
2023-07-10 11:30:55 -0400

    add rotation just bc

--- src/neural_simulator/manifold/fn.py
+++ src/neural_simulator/manifold/fn.py
@@ -1,4 +1,7 @@
 import numpy as np
+from scipy.spatial.transform import Rotation
+from scipy.stats import special_ortho_group
+from typing import Optional
 
 
 def parabolic3d(
@@ -7,6 +10,8 @@ def parabolic3d(
     y_scale: float = 1.0,
     x_center: float = 0.0,
     y_center: float = 0.0,
+    rotate: bool = False,
+    seed: Optional[int] = None,
 ):
     # embeds 2d coordinates in 3d on a parabolic surface
     # just an example for what kind of things might be in this file
@@ -15,4 +20,7 @@ def parabolic3d(
         points,
         x_scale * np.square(points[..., 0] - x_center) + y_scale * np.square(points[..., 1] - y_center),
     ], axis=-1)
+    if rotate:
+        r = special_ortho_group.rvs(dim=3, seed=seed)
+        embedded = embedded @ r
     return embedded
